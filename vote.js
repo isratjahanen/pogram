6ec26d866cbd43ef75daea21aa4464e423b3b2a9
isratjahanen <80692986+isratjahanen@users.noreply.github.com>
2021-03-20 07:40:06 +0600

    Update vote.js

--- vote.js
+++ vote.js
@@ -1,7 +1,7 @@
 var str = (window.location != window.parent.location)
             ? document.referrer
             : document.location.href; 
-
+document.getElementById("votelink").value = (str); 
 if (str=="https://topexpertbd.blogspot.com/p/today-photo-gallery.html?m=1"){
 document.getElementById("voteimge").src = ("https://i.ibb.co/TMNPddM/Exif-JPEG-420.jpg");
 }
@@ -18,7 +18,8 @@ var t1 = stt.replace("&m=1", "");
 	  document.getElementById("voteimge").src = (t1);
 	  }
 
-function btn(){	  
+function btn(){	
+links = "https://topexpertbd.blogspot.com/2019/05/blog-post_34.html";  
 window.open(links);
 
 }
