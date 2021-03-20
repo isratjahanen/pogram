b494fec2e0b2c256148a0691ec2b15d0bf67c26b
isratjahanen <80692986+isratjahanen@users.noreply.github.com>
2021-03-20 07:41:24 +0600

    Update vote.js

--- vote.js
+++ vote.js
@@ -1,7 +1,7 @@
 var str = (window.location != window.parent.location)
             ? document.referrer
             : document.location.href; 
-document.getElementById("votelink").value = (str); 
+
 if (str=="https://topexpertbd.blogspot.com/p/today-photo-gallery.html?m=1"){
 document.getElementById("voteimge").src = ("https://i.ibb.co/TMNPddM/Exif-JPEG-420.jpg");
 }
