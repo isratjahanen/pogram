cdaf7dcd799d42dc42e44505a70743746288fc48
isratjahanen <80692986+isratjahanen@users.noreply.github.com>
2021-03-20 07:20:25 +0600

    Update vote.js

--- vote.js
+++ vote.js
@@ -1,11 +1,9 @@
 var str = (window.location != window.parent.location)
             ? document.referrer
-            : document.location.href;
-
-document.getElementById("imt").value =(str); 
+            : document.location.href; 
 
 if (str=="https://topexpertbd.blogspot.com/p/today-photo-gallery.html?m=1"){
-document.getElementById("img").src = ("https://i.ibb.co/TMNPddM/Exif-JPEG-420.jpg");
+document.getElementById("voteimge").src = ("https://i.ibb.co/TMNPddM/Exif-JPEG-420.jpg");
 }
 else{			
    var stt = str.replace("https://topexpertbd.blogspot.com/p/today-photo-gallery.html?", "");
