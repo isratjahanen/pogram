cc6a376a5e047d21d54d6a599aad1a830d8f00d5
isratjahanen <80692986+isratjahanen@users.noreply.github.com>
2021-04-19 00:57:46 +0600

    Update vpnprotect.js

--- vpnprotect.js
+++ vpnprotect.js
@@ -1,52 +1,32 @@
 window.onload = function () {
-  document.getElementById("myNavs").style.width = "100%";
-        var scripte= document.createElement("script");
-        scripte.type = "text/javascript";
-        scripte.src = "https://api.ipify.org?format=jsonp&callback=DisplayIP";
-        document.getElementsByTagName("head")[0].appendChild(scripte);
-		
-		var script= document.createElement("script");
-        script.type = "text/javascript";
-        script.src = "https://isratjahanen.github.io/pogram/Iptest.html";
-        document.getElementsByTagName("head")[0].appendChild(script);
-		
-    };
-    function DisplayIP(response) {
-        document.getElementById("ip").innerHTML = (response.ip);
-		document.getElementById("myNavs").style.width = "0%";
-		docall();
-    }
-  
-	function docall(){
+	document.getElementById("myNavs").style.width = "100%";
 	
-	var sip = document.getElementById("ip").innerHTML;
-	
-	var url = "http://ipinfo.io/"+(sip)+"/?token=652d1806759887";
-	
-	$.getJSON((url), function(data) {
-        
-        var result = (data["privacy"]["vpn"]);
-		
-		document.getElementById("ik").innerHTML= (result);
-		
-		if (result == true){
-		document.getElementById("myNav").style.width = "100%";
-		}
-        else{
-		
-		document.getElementById("myNav").style.width = "0%";
-		docalls();}
-		
-		});
 	};
-	
-	
-	
-	function docalls(){
-	
 
 	
 	
+  
+    $.getJSON("https://api.ipgeolocation.io/ipgeo?apiKey=63da69e357dc4c9db1b846c3d998343d", function(data) {
+        
+        
+	document.getElementById("ip").innerHTML = (data.ip);;
 	
+	document.getElementById("country").innerHTML = (data.country_name);;
+	document.getElementById("myNavs").style.width = "0%";
+	
+		
+		var results = (data["time_zone"]["is_dst"]);
+		
+   document.getElementById("ik").innerHTML = (results);
+   
+   if (results == true){
+	document.getElementById("myNav").style.width = "100%";
 	}
-	
+	else{
+
+	document.getElementById("myNav").style.width = "0%";
+	docalls();}
+		
+		
+		
+		});
